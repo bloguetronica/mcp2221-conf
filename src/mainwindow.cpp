1704e342bf4281eeb0ddf3fea494dc1c143b85de
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2026-03-04 20:55:23 +0000

    Add files via upload

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,14 +1,61 @@
+/* MCP2221 Configurator - Version 1.0.0 for Debian Linux
+   Copyright (c) 2026 Samuel Lourenço
+
+   This program is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by the Free
+   Software Foundation, either version 3 of the License, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+   FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+   more details.
+
+   You should have received a copy of the GNU General Public License along
+   with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+   Please feel free to contact me via e-mail: samuel.fmlourenco@gmail.com */
+
+
+// Includes
+#include <QRegExp>
+#include <QRegExpValidator>
+#include "common.h"
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+// Definitions
+const int CENTRAL_HEIGHT = 171;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->lineEditVID->setValidator(new QRegExpValidator(QRegExp("[A-Fa-f\\d]+"), this));
+    ui->lineEditPID->setValidator(new QRegExpValidator(QRegExp("[A-Fa-f\\d]+"), this));
+    ui->lineEditVID->setFocus();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+    Q_UNUSED(event);
+    closeAboutDialog();  // See "common.h" and "common.cpp"
+}
+
+void MainWindow::resizeEvent(QResizeEvent *event)
+{
+    Q_UNUSED(event);
+    this->setFixedHeight(ui->menuBar->height() + CENTRAL_HEIGHT);
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    showAboutDialog();  // See "common.h" and "common.cpp"
 }
