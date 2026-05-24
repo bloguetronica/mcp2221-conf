12b2890ebc04a91ba58e01b46f78c87e1b284776
Samuel Lourenço <16304376+samuelfmlourenco@users.noreply.github.com>
2026-05-24 22:05:34 +0100

    Add files via upload

--- src/configuratorwindow.cpp
+++ src/configuratorwindow.cpp
@@ -132,6 +132,9 @@ void ConfiguratorWindow::displayUSBParameters(const MCP2221::USBParameters &usbP
 {
     ui->lineEditVID->setText(QString("%1").arg(usbParameters.vid, 4, 16, QChar('0')));  // This will autofill with up to four leading zeros
     ui->lineEditPID->setText(QString("%1").arg(usbParameters.pid, 4, 16, QChar('0')));  // Same as before
+    ui->lineEditMaxPower->setText(QString::number(2 * usbParameters.maxpow));
+    ui->lineEditMaxPowerHex->setText(QString("%1").arg(usbParameters.maxpow, 2, 16, QChar('0')));  // This will autofill with up to two leading zeros
+    ui->comboBoxPowerMode->setCurrentIndex(usbParameters.powmode);
     // TODO
 }
 
